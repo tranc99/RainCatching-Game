98177183f120ec29ee3af51274b887ebb4be6151
Ten Mutunhire <tmutunhire@gmail.com>
2015-04-23 11:45:53 +0000

    The Drop raincatching game

---
+++ core/src/com/badlogic/drop/Drop.java
@@ -0,0 +1,69 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class Drop extends ApplicationAdapter {
+	Texture img;
+
+    //my custom variables
+    private Texture dropImage;
+    private Texture bucketImage;
+    private Sound dropSound;
+    private Music rainMusic;
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Rectangle bucket;
+
+	@Override
+	public void create () {
+        // load the images for the droplet and the bucket, 64*64 pixels each
+        dropImage = new Texture(Gdx.files.internal("droplet.png"));
+        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+        // load the drop sound effect and the rain background "music"
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+        //start the playback of the background music immediately
+        rainMusic.setLooping(true);
+        rainMusic.play();
+
+        // more to come
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+		batch = new SpriteBatch();
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2;
+        bucket.y = 20;
+        bucket.width = 64;
+        bucket.height = 64;
+
+
+
+
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(bucketImage, bucket.x, bucket.y);
+        batch.end();
+
+        // more to come here
+	}
+}
